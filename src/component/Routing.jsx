76039beb88df488ffe3cdce78f34c6c00e31c93d
Asim Hasnain <124768339+asim-quencil@users.noreply.github.com>
2023-02-09 19:35:32 +0530

    policy Details page added

--- src/component/Routing.jsx
+++ src/component/Routing.jsx
@@ -2,19 +2,23 @@
 
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
 import SignIn from "./login/SignIn";
 import Policy from "./Policy/Policy";
+import PolicyDetailsMain from "./Policy/PolicyDetailsMain";
 
 const Routing = () => {
 	return (
 		
 		
 		<Routes>
+			<Route path='/*' element={<Home/>} />
 			<Route path='/auth' element={<SignIn />} />
 			<Route path='/policy' element={<Policy />} />
 			<Route path='/login/hr' element={< SignIn/>} />
 			<Route path='/login/Recruiter' element={< SignIn/>} />
 			<Route path='/login/employee' element={< SignIn/>} />
+			<Route path='/policy/policyDetails' element={< PolicyDetailsMain/>}/>
 		</Routes>
 	
 	);
